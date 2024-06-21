c29fa77bd37f58b9744891094bedd241cb8a074d
TheNebulo <amir.djaffar@outlook.com>
2024-06-21 14:08:19 +0200

    Added launcher endpoints and bug fixes

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ def check_user_exist(access_key):
     if access_key not in access_keys.keys(): return False
     return access_keys
 
-def check_game_available(access_key, game_id, game_state):
+def check_game_available(access_key, game_id, game_state=None):
     access_keys = check_user_exist(access_key)
     if not access_keys: return False
 
@@ -39,14 +39,14 @@ def get_games():
     
     games = []
     for branch in branches:
-        if branch['name'] != 'main': games.append(branch['name'])
+        if branch['name'] != 'main' and branch['name'] != 'example': games.append(branch['name'])
     
     return games
 
-async def fetch_content(session, url, is_json, return_git_json_wrapper=False):
+async def fetch_content(session: aiohttp.ClientSession, url, is_json, return_git_json_wrapper=False):
     headers = {
         "Authorization": f"token {TOKEN}",
-        "Accept": "application/vnd.github.v4+"
+        "Accept": "application/vnd.github.v4."
     }
     
     if is_json: headers["Accept"] += "json"
@@ -54,10 +54,16 @@ async def fetch_content(session, url, is_json, return_git_json_wrapper=False):
     
     async with session.get(url, headers=headers) as response:
         if response.status == 200:
-            content = await response.json()
-            if return_git_json_wrapper: return content
-            if not is_json: return content["content"]
-            return base64.b64decode(content["content"]).decode("utf-8")
+            if return_git_json_wrapper:
+                json = await response.json() 
+                return json
+            
+            if not is_json: 
+                binary_data = await response.read()
+                return base64.b64encode(binary_data).decode('utf-8')
+            
+            json = await response.json() 
+            return base64.b64decode(json["content"]).decode("utf-8")
         response.raise_for_status()
         
 def get_game_file_url(game_id, game_state, platform):
@@ -103,7 +109,7 @@ def get_game_file_url(game_id, game_state, platform):
     return game_asset_url
         
 
-async def get_game_info(game_id):
+async def get_game_info(game_id, game_state):
     async with aiohttp.ClientSession() as session:
         game_base_url = "https://api.github.com/repos/Twilight-Studios/Games/contents/{}?ref=" + game_id
         
@@ -145,7 +151,8 @@ async def get_game_info(game_id):
             "notes" : {
                 "patch_note_titles": json.loads(patch_note_titles),
                 "patch_notes": b_patch_notes
-            }
+            },
+            "state" : game_state
         }
         
         return game_info  
@@ -171,7 +178,7 @@ def validate_access():
     access_keys = check_user_exist(access_key)
     if not access_keys: abort(404)
             
-    return jsonify(access_keys[access_key])
+    return "", 200
         
 @app.route("/api/get-game", methods=["POST"])
 def get_game():
@@ -186,10 +193,36 @@ def get_game():
     game = check_game_available(access_key, game_id, game_state)
     if not game: abort(404)
             
-    game_info = asyncio.run(get_game_info(game_id))
+    game_info = asyncio.run(get_game_info(game_id, game_state))
     if not game_info: abort(404)
     
     return jsonify(game_info)
+
+@app.route("/api/get-all-games", methods=["POST"])
+def get_all_games():
+    try:
+        json_file = request.get_json()
+        access_key = json_file['key']
+    except:
+        abort(400)
+    
+    access_keys = check_user_exist(access_key)
+    if not access_keys: abort(404)
+    
+    game_infos = {}
+    games = get_games()
+    
+    for game_obj in access_keys[access_key]:
+        game_id = list(game_obj.keys())[0]
+        if game_id not in games: continue
+        game_state = game_obj[game_id]
+        
+        game_info = asyncio.run(get_game_info(game_id, game_state))
+        if not game_info: continue
+        
+        game_infos[game_id] = game_info
+    
+    return jsonify(game_infos)
 
 @app.route("/api/download-game", methods=["POST"])
 def download_game():
