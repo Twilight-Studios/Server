8423b0b0edd5065b917bd8665a07ea9ecfb66097
TheNebulo <amir.djaffar@outlook.com>
2024-09-24 14:37:06 +0200

    Added game endpoint

--- app.py
+++ app.py
@@ -46,28 +46,65 @@ def get_all_games():
 
     playtester = auth.get_playtester(playtester_id)
     if not playtester: abort(403)
+    
+    minimal = False
+    if 'minimal' in json_file:
+        if type(json_file['minimal']) == bool: 
+            minimal = json_file['minimal']
 
     game_ids = games.get_game_ids(whitelist_branches=playtester.keys())
 
-    async def fetch_game_info(game_id):
-        return await games.get_game_metadata(game_id, get_cover=True)
+    async def fetch_game_metadata(game_id):
+        if minimal: return await games.get_game_metadata(game_id, get_cover=True)
+        else: return await games.get_game_metadata(game_id, get_all=True)
 
-    async def gather_game_infos():
-        coroutines = [fetch_game_info(game_id) for game_id in game_ids]
+    async def gather_game_metadatas():
+        coroutines = [fetch_game_metadata(game_id) for game_id in game_ids]
         results = await asyncio.gather(*coroutines)
         return [result for result in results if result]
 
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    game_infos = loop.run_until_complete(gather_game_infos())
+    game_metadatas = loop.run_until_complete(gather_game_metadatas())
     loop.close()
-        
-    for game in game_infos:
-        sanitized_settings = games.sanitize_settings_metadata(game['settings'], playtester[game['id']])
-        if sanitized_settings == None: game_infos.remove(game)
-        else: game["settings"] = sanitized_settings
+    
+    sanitized_metadatas = []
+    for game_metadata in game_metadatas:
+        sanitized_metadata = games.sanitize_game_metadata(game_metadata, playtester[game_metadata['id']])
+        if sanitized_metadata != None: sanitized_metadatas.append(sanitized_metadata)
 
-    return jsonify(game_infos)
+    return jsonify(sanitized_metadatas)
+
+
+@app.route("/api/get-game", methods=["POST"]) # TODO: Add improved error validation
+def get_game():
+    try:
+        json_file = request.get_json()
+        playtester_id = json_file['playtester_id']
+        game_id = json_file['game_id']
+    except:
+        abort(400)
+
+    playtester = auth.get_playtester(playtester_id)
+    if not playtester: abort(403)
+    if game_id not in playtester: abort(406)
+    
+    minimal = False
+    if 'minimal' in json_file:
+        if type(json_file['minimal']) == bool: 
+            minimal = json_file['minimal']
+
+    async def fetch_game_metadata():
+        if minimal: return await games.get_game_metadata(game_id, get_cover=True)
+        else: return await games.get_game_metadata(game_id, get_all=True)
+
+    game_metadata = asyncio.run(fetch_game_metadata())
+    
+    sanitized_metadata = games.sanitize_game_metadata(game_metadata, playtester[game_metadata['id']])
+    if not sanitized_metadata: abort(406)
+    print(sanitized_metadata)
+
+    return jsonify(sanitized_metadata)
 
 @app.route("/updates/<path>") # Only used if launcher has custom update settings.
 def updates(path: str):
--- games.py
+++ games.py
@@ -1,13 +1,16 @@
 import aiohttp, asyncio, github
 
-def sanitize_settings_metadata(settings, available_branches):
-    branches_in_settings = list(settings['game_branches'].keys())
+def sanitize_game_metadata(metadata, available_branches):
+    for key, value in metadata['settings'].items(): metadata[key] = value
+    del metadata['settings']
     
-    for branch in branches_in_settings:
-        if branch not in available_branches: del settings['game_branches'][branch]
+    game_branches = list(metadata['game_branches'].keys())
     
-    if len(settings['game_branches'].keys()) == 0: return None
-    return settings
+    for branch in game_branches:
+        if branch not in available_branches: del metadata['game_branches'][branch]
+    
+    if len(metadata['game_branches'].keys()) == 0: return None
+    return metadata
 
 def get_game_ids(whitelist_branches: list[str] = []) -> dict[str, list]:
     branches = github.get_all_games_repo_branches()
@@ -24,19 +27,19 @@ def get_game_ids(whitelist_branches: list[str] = []) -> dict[str, list]:
 
     return games
 
-async def get_game_metadata(game_id: str, get_icon=False, get_logo=False, get_cover=False, get_background=False, get_patch_image=False, get_patch_notes=False):
+async def get_game_metadata(game_id: str, get_all=False, get_icon=False, get_logo=False, get_cover=False, get_background=False, get_patch_image=False, get_patch_notes=False):
     async with aiohttp.ClientSession() as session:
         files_to_fetch = [{"name": "settings", "path" : "settings.json"}]
         
-        if get_icon: files_to_fetch.append({"name": "icon", "path" : "art/icon.png"})
-        if get_logo: files_to_fetch.append({"name": "logo", "path" : "art/logo.png"})
-        if get_cover: files_to_fetch.append({"name": "cover", "path" : "art/cover.png"})
-        if get_background: files_to_fetch.append({"name": "background", "path" : "art/background.png"})
-        if get_patch_image: files_to_fetch.append({"name": "patch", "path" : "art/patch.png"})
+        if get_icon or get_all: files_to_fetch.append({"name": "icon", "path" : "art/icon.png"})
+        if get_logo or get_all: files_to_fetch.append({"name": "logo", "path" : "art/logo.png"})
+        if get_cover or get_all: files_to_fetch.append({"name": "cover", "path" : "art/cover.png"})
+        if get_background or get_all: files_to_fetch.append({"name": "background", "path" : "art/background.png"})
+        if get_patch_image or get_all: files_to_fetch.append({"name": "patch", "path" : "art/patch.png"})
         
         non_patch_note_content_amount = len(files_to_fetch)
         
-        if get_patch_notes:
+        if get_patch_notes or get_all:
             patch_note_urls = await github.load_file_async("notes", session, branch=game_id, return_github_json_wrapper=True)
             for note in patch_note_urls: files_to_fetch.append({"name": "note", "path" : f"notes/{note['name']}"})
 
@@ -44,7 +47,7 @@ async def get_game_metadata(game_id: str, get_icon=False, get_logo=False, get_co
         results = await asyncio.gather(*coroutines)
         
         patch_notes = None
-        if get_patch_notes: 
+        if get_patch_notes or get_all: 
             patch_notes = results[non_patch_note_content_amount:]
             results = results[:non_patch_note_content_amount]
         
@@ -52,7 +55,7 @@ async def get_game_metadata(game_id: str, get_icon=False, get_logo=False, get_co
         for index, data in enumerate(results):
             metadeta[files_to_fetch[index]["name"]] = data
             
-        if get_patch_notes: metadeta["patch_notes"] = patch_notes
+        if get_patch_notes or get_all: metadeta["patch_notes"] = patch_notes
         metadeta['id'] = game_id
         
         return metadeta
